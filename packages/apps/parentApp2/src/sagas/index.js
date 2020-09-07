cefdc4457378fda1351703a45c15ad7c2c0c6629
Aislinn Hayes <aislinn.hayes@hmhco.com>
2020-09-07 12:01:50 +0100

    Get rid of the extra packages so that we can use one parent and one child to try and debug the memory leaks in the tests

--- packages/apps/parentApp2/src/sagas/index.js
+++
@@ -1,9 +0,0 @@
-import { all, takeLatest } from 'redux-saga/effects';
-import { BOG_ACTIONS } from '../actions/types';
-import { fetchBogDetails } from './getBog.saga';
-
-export default function*() {
-  yield all([
-    takeLatest(BOG_ACTIONS.FETCH_BOG_DETAILS, fetchBogDetails),
-  ]);
-}
